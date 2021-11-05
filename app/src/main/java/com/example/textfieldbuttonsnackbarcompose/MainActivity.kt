1e83da7c05530f380d1e7d2a88c17241c3a2a0f1
Sagar <avhad.avhad@gmail.com>
2021-11-05 12:35:43 +0530

    Understood cool way to use remember. We can use delegate.

--- app/src/main/java/com/example/textfieldbuttonsnackbarcompose/MainActivity.kt
+++ app/src/main/java/com/example/textfieldbuttonsnackbarcompose/MainActivity.kt
@@ -3,18 +3,44 @@ package com.example.textfieldbuttonsnackbarcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.textfieldbuttonsnackbarcompose.ui.theme.TextFieldButtonSnackBarComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember{mutableStateOf("")}
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(30.dp)
+                ) {
+
+                    TextField(value =textFieldState , onValueChange = {
+                        textFieldState = it
+                    } )
+                }
+            }
+
         }
     }
 }
